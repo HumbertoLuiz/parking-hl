c062b01fe01e59b6e4ba83e9f8c5f1a2c9cb03a0
HumbertoLuiz <kaos_132@hotmail.com>
2022-08-03 17:37:43 -0300

    estrutura do projeto

---
+++ parking/src/main/java/io/github/humbertoluiz/parking/controller/HelloController.java
@@ -0,0 +1,15 @@
+package io.github.humbertoluiz.parking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+	@GetMapping
+	public String Hello() {
+		return "Hello!!";
+	}
+}
