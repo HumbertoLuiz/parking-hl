0463c045ba7ff022aeefe3965ed33776c384cae3
HumbertoLuiz <kaos_132@hotmail.com>
2022-08-08 13:41:13 -0300

    Basic security

--- parking/src/main/java/io/github/humbertoluiz/parking/config/SwaggerConfig.java
+++ parking/src/main/java/io/github/humbertoluiz/parking/config/SwaggerConfig.java
@@ -1,48 +1,64 @@
 package io.github.humbertoluiz.parking.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableWebMvc
+@Component
 @EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfig {
 
     @Bean
     public Docket getDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()                
+                .select()
                 .apis(RequestHandlerSelectors.basePackage("io.github.humbertoluiz.parking"))
-                .paths(PathSelectors.any())
                 .build()
-                .apiInfo(metaData());
-//                .securityContexts(Arrays.asList(getSecurityContext()))
-//                .securitySchemes(Arrays.asList(basicAuthScheme()));
-
+                .apiInfo(metaData())
+                .securityContexts(Arrays.asList(actuatorSecurityContext()))
+                .securitySchemes(Arrays.asList(basicAuthScheme()));
     }
 
-//    private SecurityScheme basicAuthScheme() {
-//        return new BasicAuth("basicAuth");
-//    }
-//
-//    private SecurityContext getSecurityContext() {
-//        return SecurityContext.builder()
-//                .securityReferences(Arrays.asList(basicAuthReference()))
-//                .build();
-//    }
-//
-//    private SecurityReference basicAuthReference() {
-//        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
-//    }
+    private SecurityContext actuatorSecurityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Arrays.asList(basicAuthReference()))
+                .build();
+    }
+
+    private SecurityScheme basicAuthScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityReference basicAuthReference() {
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+    }
+
+    private List<SecurityScheme> basicScheme() {
+        List<SecurityScheme> schemeList = new ArrayList<>();
+        schemeList.add(new BasicAuth("basicAuth"));
+        return schemeList;
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("apiKey", "Authorization", "header");
+    }
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
